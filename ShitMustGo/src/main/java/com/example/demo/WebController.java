688a02eafdff27d25880ee441449ffb092e87795
LucasCurry <123386797+LucasCurry@users.noreply.github.com>
2023-03-27 15:23:36 +0200

    Added some templates, a start to the homepage and style.css file. New Icon aswell!
    Signed-off-by: LucasCurry <lucas_curry@hotmail.com>

--- ShitMustGo/src/main/java/com/example/demo/WebController.java
+++ ShitMustGo/src/main/java/com/example/demo/WebController.java
@@ -1,21 +1,25 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    @Autowired
+    AccountRepo accountRepo;
 
     @GetMapping("/")
     String home(Model model) {
-        model.addAttribute("tasks", );
+        Account account = accountRepo.findById(1L).get();
+        model.addAttribute("username", account.getUsername());
 
-        return "Home";
+        return "home";
     }
 
     @GetMapping("/secret")
     String secret() {
-        return "Secret";
+        return "secret";
     }
 }
