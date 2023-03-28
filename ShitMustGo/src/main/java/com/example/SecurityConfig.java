a803793162ea7e4cb8c9c7b9f2bdd5eb32bd3ad2
LucasCurry <lucas.curry@brightstraining.com>
2023-03-28 15:22:18 +0200

    Added some more spring security features regarding logging in the accounts etc. Test 1 of Thege/Curry branch

--- ShitMustGo/src/main/java/com/example/SecurityConfig.java
+++ ShitMustGo/src/main/java/com/example/SecurityConfig.java
@@ -1,39 +1,69 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityUserDetailsService userDetailsService;
+
     //Configure Security
     @Bean
     @Order(1)
     public SecurityFilterChain auth0FilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+        http.authorizeHttpRequests().requestMatchers(AntPathRequestMatcher.antMatcher("/h2/**")).permitAll();
+        http.userDetailsService(userDetailsService);
+
         http
                 .authorizeHttpRequests()
-                .requestMatchers("/**","/h2**","/h2/**").permitAll()
+                .requestMatchers("/","/login","/register", "/css/**", "/images/**" ).permitAll()
                 .anyRequest().authenticated();
                 http.csrf().disable();
                 http.headers().frameOptions().disable();
+        http.formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll();
+        http.logout().logoutSuccessUrl("/");
+
         return http.build();
     }
 
 
     //Users
-    @Bean
+  /*  @Bean
     public UserDetailsService userDetailsService() {
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
         manager.createUser(User.withDefaultPasswordEncoder().username("user").password("123").roles("USER").build());
         manager.createUser(User.withDefaultPasswordEncoder().username("admin").password("123").roles("USER","ADMIN").build());
         return manager;
+    }*/
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(11);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
