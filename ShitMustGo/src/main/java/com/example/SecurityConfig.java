dce8edf5d6aa163850ea1e52aed3cfb8bc74aada
Jonat <jonathan.thege@hotmail.com>
2023-04-06 14:29:06 +0100

    Added index to web sec

--- ShitMustGo/src/main/java/com/example/SecurityConfig.java
+++ ShitMustGo/src/main/java/com/example/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests()
-                .requestMatchers("/","/login","/register", "/css/**", "/images/**", "/task/**", "/contact/**", "/customerservice/**", "/faq/**","/hello/**","/webjars/**","/gs-guide-websocket/**", "/home/**", "/home2/**").permitAll()
+                .requestMatchers("/","/login","/register", "/css/**", "/images/**", "/task/**", "/contact/**", "/customerservice/**", "/faq/**","/hello/**","/webjars/**","/gs-guide-websocket/**", "/home/**", "/home2/**", "/index/**").permitAll()
                 .anyRequest().authenticated();
         http
                 .formLogin()
